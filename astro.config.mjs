0b93b4eb24479733cbdc16c1133cd8764495925b
Roberto <rodri.riobo@gmail.com>
2024-01-12 16:34:04 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+
 export default defineConfig({
-
-
-    export default defineConfig({
-        site: 'https://rodririobo.github.io/Astro/',
-        base: '/',
-      })
-
-
-});
+  site: 'https://rodririobo.github.io/Astro/',
+  base: '/',
+})
